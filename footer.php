16c2fb2b31a70b4ef7c14fb901f03cbc9e8d87f3
Derek Eder <derek.eder@gmail.com>
2012-09-18 10:42:39 -0500

    removed unused images

--- footer.php
+++ footer.php
@@ -15,8 +15,6 @@
 						<?php bones_footer_links(); // Adjust using Menus in Wordpress Admin ?>
 					</nav>
 					
-					<p class="pull-right"><a href="http://320press.com" id="credit320" title="By the dudes of 320press">320press</a></p>
-			
 					<p class="attribution">&copy; <?php bloginfo('name'); ?></p>
 				
 				</div> <!-- end #inner-footer -->
