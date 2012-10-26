97affd3e9dec79c74bf5a7846461e8e0a339d14f
Derek Eder <derek.eder@gmail.com>
2012-10-26 14:31:05 -0500

    moved home page content around, changed row markup

--- index.php
+++ index.php
@@ -49,51 +49,8 @@
 							</div>
 						</div>
 
-				<div class="span6">
-					<h2>Learn</h2>
-					<P>The free courses offered through Connect Chicago and its partners can assist all Chicago residents of 
-						all ages and skills with training covering topics such as:</p>
-
-					<ul>
-		        <li>Basic digital literacy</li>
-		        <li>Desktop applications</li>
-		        <li>Online job searching</li>
-		        <li>Resume creation</li>
-		        <li>Social media</li>
-		        <li>Online personal financial management</li>
-		       </ul>
-
-					<p><a href='/learn'>More &raquo;</a></p>
-					<!-- <ul>
-						<li>Learn to create your own resume</li>
-						<li>Get training on Microsoft Word and Excel</li>
-						<li>Get connected to your friends and family by using social media</li>
-					</ul> -->
-				</div>
-			</div>
-
-			<div id="content" class="clearfix row-fluid">
-				<div class="span6 clearfix">
-					<h2>About Connect Chicago</h2>
-					<p>Connect Chicago is a network of sites that spans the city and offers broadband access to Chicago’s 
-						three million residents. With sites in nearly every neighborhood, Connect Chicago is reaching 
-						populations with limited access to digital tools and knowledge.</p>
-
-						<p>Connecting six partnering organizations – City Colleges of Chicago, Chicago Housing Authority, 
-						Chicago Public Libraries, City of Chicago’s Department of Family Support Services, Smart Health 
-						Centers and Chicago Cook Workforce Partnership – with each other, Connect Chicago has created a wide-reaching community of public computer 
-						centers, offering every Chicagoan the use of more than 3,000 free computers in 175 locations, a 
-						variety of free computer training courses in more than 70 venues, and more than 2,300 free WiFi 
-						hotspots and free internet access sites.</p>
-
-						<p>We welcome everyone to join the digital community by taking advantage of these free resources 
-						and public computing centers. Getting connected will help you achieve your goals—benefiting you, 
-						your family and your community—no matter where you are.</p>
-
-						<p><a href='/about'>Read more &raquo;</a></p>
-				</div>
 				<div id="main" class="span6 clearfix" role="main">
-					<h2>Recent News &amp; Events</h2>
+					<h2>News &amp; Events</h2>
 					<?php
 						query_posts('cat=1&posts_per_page=3&orderby=date&order=DESC');
 						if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
@@ -115,6 +72,50 @@
 					<p><a href='/news-events'>Read more &raquo;</a></p>
 			
 				</div> <!-- end #main -->
+			</div>
+
+			<div id="content" class="clearfix row">
+				<div class="span6">
+					<h2>Learn</h2>
+					<P>The free courses offered through Connect Chicago and its partners can assist all Chicago residents of 
+						all ages and skills with training covering topics such as:</p>
+
+					<ul>
+		        <li>Basic digital literacy</li>
+		        <li>Desktop applications</li>
+		        <li>Online job searching</li>
+		        <li>Resume creation</li>
+		        <li>Social media</li>
+		        <li>Online personal financial management</li>
+		       </ul>
+
+					<p><a href='/learn'>More &raquo;</a></p>
+					<!-- <ul>
+						<li>Learn to create your own resume</li>
+						<li>Get training on Microsoft Word and Excel</li>
+						<li>Get connected to your friends and family by using social media</li>
+					</ul> -->
+				</div>
+
+				<div class="span6 clearfix">
+					<h2>About Connect Chicago</h2>
+					<p>Connect Chicago is a network of sites that spans the city and offers broadband access to Chicago’s 
+						three million residents. With sites in nearly every neighborhood, Connect Chicago is reaching 
+						populations with limited access to digital tools and knowledge.</p>
+
+						<p>Connecting six partnering organizations – City Colleges of Chicago, Chicago Housing Authority, 
+						Chicago Public Libraries, City of Chicago’s Department of Family Support Services, Smart Health 
+						Centers and Chicago Cook Workforce Partnership – with each other, Connect Chicago has created a wide-reaching community of public computer 
+						centers, offering every Chicagoan the use of more than 3,000 free computers in 175 locations, a 
+						variety of free computer training courses in more than 70 venues, and more than 2,300 free WiFi 
+						hotspots and free internet access sites.</p>
+
+						<p>We welcome everyone to join the digital community by taking advantage of these free resources 
+						and public computing centers. Getting connected will help you achieve your goals—benefiting you, 
+						your family and your community—no matter where you are.</p>
+
+						<p><a href='/about'>Read more &raquo;</a></p>
+				</div>
     
 			</div> <!-- end #content -->
 
